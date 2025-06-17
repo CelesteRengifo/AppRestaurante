1e791a07cc58498c019fe6fc3404407a7d2f15e8
Gael Mozombite <kingfire9814@gmail.com>
2025-06-16 21:52:55 -0500

    Cambie la ruta de inicio

--- AppRestaurante/App.xaml.cs
+++ AppRestaurante/App.xaml.cs
@@ -6,7 +6,7 @@ namespace AppRestaurante
         public App()
         {
             InitializeComponent();
-            MainPage = new AppRestaurante.Paginas.Usuario.Registrar();
+            MainPage = new InicioSesion();
         }
     }
 }
