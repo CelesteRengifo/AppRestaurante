7618b7f084563fcff154f4d739f7b152cdf54880
Gael Mozombite <kingfire9814@gmail.com>
2025-06-30 02:44:00 -0500

    Hola buenas noches señora Any Celeste Alva Rengifo....vine a decirle tengo sueño crjo...solo implemente las paginas adminpage y meseropage, más no el logueo.

--- AppRestaurante/App.xaml.cs
+++ AppRestaurante/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AppRestaurante.Paginas.Inventario;
+using AppRestaurante.Paginas.Logueo;
 using AppRestaurante.Paginas.Usuario;
 namespace AppRestaurante
 {
@@ -7,8 +8,9 @@ namespace AppRestaurante
         public App()
         {
             InitializeComponent();
-            MainPage = new CambiarContrasenaPage();
-            //MainPage = new NavigationPage(new Paginas.Usuario.Editar());
+            //MainPage = new InicioSesion();
+            MainPage = new NavigationPage(new MeseroPage());
+            //MainPage = new NavigationPage(new Paginas.Usuario.EditarUsuarioPage());
             //MainPage = new AppRestaurante.Paginas.Usuario.Registrar();
             //MainPage = new AppRestaurante.Paginas.Usuario.Menu();
         }
---
+++ AppRestaurante/Paginas/Logueo/AdminPage.xaml.cs
@@ -0,0 +1,28 @@
+namespace AppRestaurante.Paginas.Logueo;
+
+public partial class AdminPage : ContentPage
+{
+	public AdminPage()
+	{
+		InitializeComponent();
+	}
+    private async void IrAGestionarUsuarios(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Paginas.Usuario.Editar());
+    }
+
+    private async void IrAGestionarRoles(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Paginas.Usuario.GestionarRoles());
+    }
+
+    private async void IrARegistrarUsuario(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Paginas.Usuario.Registrar());
+    }
+
+    private async void IrARegistrarRol(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Paginas.Usuario.RegistrarRol());
+    }
+}
