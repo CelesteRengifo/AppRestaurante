97d9cd9eb1cb9777e3aabeea385dd6622215aea9
Anny Celeste Peñaherrera Rengifo <2225254@unapiquitos.edu.pe>
2025-07-07 17:02:47 -0500

    union con experimento

--- AppRestaurante/App.xaml.cs
+++ AppRestaurante/App.xaml.cs
@@ -9,12 +9,12 @@ namespace AppRestaurante
         {
             InitializeComponent();
             SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCeUx3QXxbf1x1ZFdMYVhbRnJPIiBoS35Rc0VkWXhfdHVRRGRYVUB3VEFd");
-            //MainPage = new AppShell();
+            MainPage = new AppShell();
             //MainPage = new AppRestaurante.Paginas.Usuario.InicioSesion();
             //MainPage = new NavigationPage(new AdminPage());
             //MainPage = new NavigationPage(new Paginas.Usuario.EditarUsuarioPage());
             //MainPage = new AppRestaurante.Paginas.Usuario.Registrar();
-            MainPage = new NavigationPage(new ReporteVentasPage());
+            //MainPage = new NavigationPage(new ReporteVentasPage());
 
         }
     }
